1379b5987a8c5850e905f70b707c9c9fb4a04e03
YulyKo <juliakotenko.forstudy@gmail.com>
2019-11-25 13:53:14 +0200

    task-4: revert seceleton

--- homeworks/julia.kotenko_YulyKo/homework_4/src/index.js
+++ homeworks/julia.kotenko_YulyKo/homework_4/src/index.js
@@ -1,64 +1,9 @@
-import formValidation from 'FormValidation';
+import _ from 'lodash';
 
-class LoginForm {
-    constructor() {
-        this.form = document.querySelector('#form');
-        this.firstName = this.form.querySelector('#firstName').value;
-        this.secondName = this.form.querySelector('#secondName').value;
-        this.email = this.form.querySelector('#email').value;
-        this.phone = this.form.querySelector('#phone').value;
-        this.age = this.form.querySelector('#age').value;
-        this.messageZone = this.form.getElementsById('message');
-        document.getElementsById('#submit').addEvantListener('submit', this.onSubmit.bind(this));
-    }
-
-    setMassage(message) {
-        this.messageZone.innerHTML = message;
-    }
-
-    checkValidData(data, nameData) {
-        if (!data) {
-            message = 'Your' +  nameData + 'not valid.';
-            this.setMassage(message);
-        }
-    }
-
-    isValidFirstName(name) {
-        formValidation.setFirstName(name);
-        firstNameValid = formValidation.validateName(name);
-        this.checkValidData(firstNameValid, 'first name');
-    }
-
-    isValidSecondName(name) {
-        formValidation.setFirstName(name);
-        secondNameValid = formValidation.validateName(name);
-        this.checkValidData(firstNameValid, 'second name');
-    }
-
-    isValidEmail(email) {
-        formValidation.setEmail(email);
-        emailValid = formValidation.valideteEmail();
-        this.checkValidData(emailValid, 'email');
-    }
-
-    isValidPhone(phone) {
-        formValidation.setPhone(phone);
-        phoneValid = formValidation.isValidPhone();
-        this.checkValidData(phoneValid, 'phone');
-    }
-
-    isValidAge(age) {
-        formValidation.setAge(age);
-        ageValid = formValidation.isValidAge();
-        this.checkValidData(ageValid, 'age');
-    }
-
-    onSubmit() {
-        event.preventDefault();
-        this.isValidFirstName(this.firstName);
-        this.isValidSecondName(this.secondName);
-        this.isValidPhone(this.phone);
-        this.isValidEmail(this.email);
-        this.isValidAge(this.age);
-    }
+function component() {
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
 }
+
+document.body.appendChild(component());
--- homeworks/julia.kotenko_YulyKo/homework_4/src/regulars.js
+++
@@ -1,7 +0,0 @@
-export class Regulars {
-    EMAIL_FORMAT = /\S+@\s+\.\S+/;
-    NAME_FORMAT = /^[A-Za-z\s]+$/;
-    PHONE_FORMAT = /^\+?3?8?(0\d{9})$/;
-        // \+38(\+([0-9]{2,3})+\(\+([0-9]{3})+\-\+([0-9]{2})+\-\+([0-9]{3}))
-
-}
