8a854d5a0d388c0e09a330ac8e1bacd2460792eb
YulyKo <juliakotenko.forsudy@gmail.com>
2019-12-05 17:05:07 +0200

    task-4: fixed regular for email

--- homeworks/julia.kotenko_YulyKo/homework_4/src/Regulars.js
+++ homeworks/julia.kotenko_YulyKo/homework_4/src/Regulars.js
@@ -1,3 +1,3 @@
-export const EMAIL_FORMAT = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+export const EMAIL_FORMAT = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 export const PHONE_FORMAT = /(?:\+38\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2})/;
 export const NAME_FORMAT = /^[A-Za-z\s]+$/;
